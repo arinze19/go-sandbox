d8b0470a1f7ad4c4737d426b08c2a54b659a51b7
Arinze Obi <arinze19@outlook.com>
2024-08-24 19:57:26 +0100

    functions

--- main.go
+++ main.go
@@ -4,23 +4,23 @@ import "fmt";
 
 
 func main() {
-	/**
-	* Declaring mutliple varialbes 
-	*/
-	// var (
-	// 	foo string = "Hello"
-	// 	bar string = "World"
-	// )
-	// password := "password"
-	// const PI = 3.14
-	percent := (7.0 / 9) * 100
-	sentence := fmt.Sprintf("hex:%x bin:%b", 10 ,10)
-	msg := `This is a 
-		multiline message that comes from my 
-		heart and also a strange place
-	`
+	// result := myFunc("Arinze", "Obi")
+	// result := add(1, 2, 3, 4, 5)
+	defer fmt.Println("This is the last line of the main function: Thanks to defer")
+	defer fmt.Println("This is the second to the last line of the main function: Thanks to defer")
+	fmt.Println("This is the first line of the main function")
+}
 
-	fmt.Printf("Percent: %.2f %%\n", percent)
-	fmt.Print(sentence, "\n")
-	fmt.Print(msg)
+func myFunc (argument1 string, argument2 string) string {
+	msg := fmt.Sprintf("Hello %s %s", argument1, argument2)
+	return msg
+}
+
+func add(values ...int) int {
+	total := 0;
+	for i := 0; i < len(values); i++ {
+		total += values[i]
+	}
+
+	return total
 }
