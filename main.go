9639b16ac86a556c3fc49d5ef517df06ab32977b
Arinze Obi <arinze19@outlook.com>
2025-02-04 23:18:37 +0100

    pointers

--- main.go
+++ main.go
@@ -1,35 +1,43 @@
 package main
 
-import "fmt";
-
+import "fmt"
 
 func main() {
 	// result := myFunc("Arinze", "Obi")
 	// result := add(1, 2, 3, 4, 5)
 	// defer fmt.Println("This is the last line of the main function: Thanks to defer")
 	// defer fmt.Println("This is the second to the last line of the main function: Thanks to defer")
-	DeploymentOptions := [3]string {"Development", "Staging", "Production"}
+	age := 35
 
-	for index, value := range DeploymentOptions {
-		fmt.Println(index, value)
-	}
+	getAdultYears(&age)
 
-	for i := 0; i < len(DeploymentOptions); i++ {
-		fmt.Println(DeploymentOptions[i])
-	}
+	fmt.Println("Age:", age)
 }
 
-
-func myFunc (argument1 string, argument2 string) string {
-	msg := fmt.Sprintf("Hello %s %s", argument1, argument2)
-	return msg
+func getAdultYears(age *int) {
+	*age = *age - 18
 }
 
-func add(values ...int) int {
-	total := 0;
-	for i := 0; i < len(values); i++ {
-		total += values[i]
-	}
+// DeploymentOptions := [3]string{"Development", "Staging", "Production"}
 
-	return total
-}
+// for index, value := range DeploymentOptions {
+// 	fmt.Println(index, value)
+// }
+
+// for i := 0; i < len(DeploymentOptions); i++ {
+// 	fmt.Println(DeploymentOptions[i])
+// }
+
+// func myFunc (argument1 string, argument2 string) string {
+// 	msg := fmt.Sprintf("Hello %s %s", argument1, argument2)
+// 	return msg
+// }
+
+// func add(values ...int) int {
+// 	total := 0;
+// 	for i := 0; i < len(values); i++ {
+// 		total += values[i]
+// 	}
+
+// 	return total
+// }
